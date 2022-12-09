82ff224912f2953a8ef6cb1d17864b6d1136a642
icaioreis <caioreis3186@gmail.com>
2022-12-08 22:07:07 -0300

    NODEMON

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.json())
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!aaaa')
 })
 
 app.listen(port, () => {
