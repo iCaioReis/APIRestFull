55ad0ea59e76b63d93618308d13f2969406a38b5
icaioreis <caioreis3186@gmail.com>
2022-12-11 22:39:12 -0300

    CRUD

--- index.js
+++ index.js
@@ -2,13 +2,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyparser = require('body-parser')
+const usuarios = []
 
 app.use(bodyparser.urlencoded({extended: true}))
 app.use(bodyparser.json())
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!aaaa')
+app.get('/usuarios', (req, res) => {
+  res.send(usuarios)
+})
+
+app.put('/usuarios/:id', (req,res) => {
+  const index = usuarios.findIndex(user => user.id === ~~req.params.id)
+  const usuario = req.body
+
+  usuarios.splice(index,1, usuario)
+  res.send(usuario)
+})
+
+app.post('/usuarios', (req,res) => {
+  const usuario = req.body
+
+  usuarios.push(usuario)
+  res.send(usuario)
+})
+
+app.delete('/usuarios/:id', (req,res) => {
+  const index = usuarios.findIndex(user => user.id === ~~req.params.id)
+  usuarios.splice(index, 1)
+  res.send('Usuário deletado com sucesso!')
 })
 
 app.listen(port, () => {
